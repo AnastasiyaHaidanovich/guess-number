f292bc38b8b111d3c7d7e5b448cd523def8912f7
AnastasiyaHaidanovich <Nastyal0ve9517@gmail.com>
2021-12-16 16:16:00 +0300

    fix: mistakes

--- script.js
+++ script.js
@@ -1,47 +1,48 @@
 
-let computerNum = Math.floor(Math.random() * 99) + 1;
 const attempts = 10;
 let userAttempts = 0;
 
 const isNumber = function (num) {
     return !isNaN(parseFloat(num)) && isFinite(num);
 };
-
-const guessNum = function () {
-    let userNum = prompt("Угадай число от 1 до 100");
-    function compareNum () {
-        if (userNum == computerNum){
-            if (confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?")) {
-                userAttempts = 0;
-                guessNum();
-            } else {
-                alert("Спасибо за игру!");
-            }
-        } else if (userAttempts == 9){
-            if (confirm("Попытки закончились, хотите сыграть еще?")) {
-                userAttempts = 0;
-                guessNum();
-            } else {
-                alert("Спасибо за игру!");
-            }
-        } else if (userNum === "" || userNum === " " || isNaN(Number(userNum))) {
-            userNum = prompt("Введи число!!");
-            compareNum();
-        } else if (!isNumber(userNum)) {
-            alert("Игра окончена");
-        } else if (userNum > computerNum){
-            userAttempts++;
-            alert("Загаданное число меньше, осталось попыток " + (attempts - userAttempts));
-            userNum = prompt("Введи новое число");
-            compareNum();
-        } else if (userNum < computerNum) {
-            userAttempts++;
-            alert("Загаданное число больше, осталось попыток " + (attempts - userAttempts));
-            userNum = prompt("Введи новое число");
-            compareNum();     
-        } 
-    }
-    compareNum();
+const letsPlay = function(){
+    let computerNum = Math.floor(Math.random() * 99) + 1;
+    const guessNum = function () {
+        let userNum = prompt("Угадай число от 1 до 100");
+        function compareNum () {
+            if (userNum == computerNum){
+                if (confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?")) {
+                    userAttempts = 0;
+                    letsPlay();
+                } else {
+                    alert("Спасибо за игру!");
+                }
+            } else if (userAttempts == 9){
+                if (confirm("Попытки закончились, хотите сыграть еще?")) {
+                    userAttempts = 0;
+                    letsPlay();
+                } else {
+                    alert("Спасибо за игру!");
+                }
+            } else if (userNum.trim() === "" || isNaN(Number(userNum))) {
+                userNum = prompt("Введи число!!");
+                compareNum();
+            } else if (!isNumber(userNum)) {
+                alert("Игра окончена");
+            } else if (userNum > computerNum){
+                userAttempts++;
+                alert("Загаданное число меньше, осталось попыток " + (attempts - userAttempts));
+                userNum = prompt("Введи новое число");
+                compareNum();
+            } else if (userNum < computerNum) {
+                userAttempts++;
+                alert("Загаданное число больше, осталось попыток " + (attempts - userAttempts));
+                userNum = prompt("Введи новое число");
+                compareNum();     
+            } 
+        }
+        compareNum();
+    };
+    guessNum();
 };
-
-guessNum();
+letsPlay();
